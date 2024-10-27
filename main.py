52d2d36e2d91b0a136b4e9218f7c76de2866c700
thrashdev <thrashdev@protonmail.com>
2024-10-27 20:32:27 +0500

    fixed window, added geometry

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+from geometry import Line, Point
 from window import Window
 
 
 def main():
     win = Window(800,600)
+    l1 = Line(Point(100, 100), Point(200, 200))
+    l2 = Line(Point(400, 200), Point(0, 100))
+    win.draw_line(l1, "red")
+    win.draw_line(l2, "blue")
     win.wait_for_close()
 
 
